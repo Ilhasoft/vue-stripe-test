37b422a2ca6dbcfc57a2870f4f3a351fb5054b6e
Daniel Yohan <dyohan9@gmail.com>
2021-08-12 08:48:53 -0300

    Updated app.vue

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,13 @@
 
 <script>
 // import { StripeElementCard } from '@vue-stripe/vue-stripe'
-import axios from 'axios'
+// import axios from 'axios'
 export default {
   components: {
     // StripeElementCard
   },
   data () {
-    this.pulishableKey = process.env.VUE_APP_STRIPE_PUBLISHABLE_KEY
+    this.pulishableKey = 'pk_test_swApzrxK2K7euK0hDCykGfk600vMN1Uot0'
     return {
       token: null,
       clientSecret: null,
@@ -25,21 +25,22 @@ export default {
     }
   },
   mounted () {
-    axios({
-      method: 'POST',
-      url: 'https://922b15145e0a.ngrok.io/create-setup-intent',
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
-    }).then(res => {
-      this.clientSecret = res.data.client_secret
-      const elements = this.$stripe.elements()
+    // axios({
+    //   method: 'POST',
+    //   url: 'http://127.0.0.1:8000/v1/organization/org/invoice/setup_intent/5320b465-e810-468d-ba47-2a2f3666b0f6/',
+    //   headers: {
+    //     'Access-Control-Allow-Origin': '*',
+    //   }
+    // }).then(res => {
+    // this.clientSecret = res.data.client_secret
+    this.clientSecret = 'seti_1JK2gTH0GXB0U1FI0Jz9InBA_secret_Jxydx5VGKYhbmqNFoBaCBVoiPIdPNw4'
+    const elements = this.$stripe.elements()
 
-      this.cardSaved = elements.create('card', {})
-      this.cardSaved.mount('#test')
+    this.cardSaved = elements.create('card', {})
+    this.cardSaved.mount('#test')
 
-      console.log(this.clientSecret)
-    }).catch(err => console.log(err))
+    console.log(this.clientSecret)
+    // }).catch(err => console.log(err))
   },
   methods: {
     submit () {
